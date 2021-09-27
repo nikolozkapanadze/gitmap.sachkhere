c84a0450961b4d43185ce931229d5e9a81d9554f
nikolozkapanadze <nikoloz.kapanadze691@ens.tsu.edu.ge>
2021-09-27 13:03:20 +0400

    Initial commit

---
+++ data/sachxerepoint_4.js
@@ -0,0 +1,1 @@
+var json_sachxerepoint_4 = {"type":"FeatureCollection","name":"sachxerepoint_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"Id":0.0,"dasaxeleba":null},"geometry":{"type":"Point","coordinates":[43.407018338695536,42.340250889002292]}}]}
